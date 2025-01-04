89a223a44a798518103cf939ec75e437719dacbe
Paolo <pvizconded@hotmail.com>
2025-01-04 00:25:42 -0500

    Probando

--- Pruebas/MayusAMin.java
+++
@@ -1,6 +0,0 @@
-public class MayusAMin {
-    public String cambioMayus(String palabra) {
-        String cambio = palabra.toUpperCase();
-        return cambio;
-    }
-}
--- Pruebas/prueba.java
+++
@@ -1,9 +0,0 @@
-public class prueba {
-    public static void main(String[] args) {
-        MayusAMin a = new MayusAMin();
-
-        String cambiada = a.cambioMayus("indica a Hibernate que use el dialecto SQL específico para PostgreSQL");
-
-        System.out.println(cambiada);
-    }
-}
